00084ebb74eccd97a2ad4158c0d893bb24b9cf06
Bitura <muxtorovmurodbek007@gmail.com>
2025-10-30 05:14:33 -0700

    Initial commit(1-8)

--- problem8.cpp
+++ problem8.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
 using namespace std;
 
+char to_lower(const char c) {
+    if (c >= 'A' && c <= 'Z') {
+        return c + ('a' - 'A');
+    }
+    return c;
+}
 
+void CountChar(string s) {
+    int vcount = 0, ccount = 0;
+    for (int i = 0; i < s.length(); i++) {
+        if (to_lower(s[i])>=97 && to_lower(s[i])<=122) {
+            if (to_lower(s[i]) == 97 || to_lower(s[i]) == 101 ||to_lower(s[i]) == 105 ||to_lower(s[i]) == 111 || to_lower(s[i]) == 117) {
+                vcount++;
+            }
+            else {
+                ccount++;
+            }
+        }
+    }
+    cout<<"Vowels: "<<vcount<<endl;
+    cout<<"Consonants: "<<ccount<<endl;
+    }
 
 int main() {
-
+    string s;
+    getline(cin, s);
+    CountChar(s);
     return 0;
 }
